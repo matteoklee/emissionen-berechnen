6d900e3f28e719a840b09f2a765ecab998bdb320
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2024-12-19 00:35:45 +0100

    init.sh keycloak ssl

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Waiting for Keycloak to start..."
+sleep 5
+
+docker exec -it keycloak /bin/bash -c '
+cd /opt/keycloak/bin
+./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
+./kcadm.sh update realms/master -s sslRequired=NONE
+'
+
+echo "Keycloak initialization completed!"
